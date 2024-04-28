9954bfe831b4a1d5ceaa2bd1ede49a917bfeb12a
lulu-cao <lulu.cao.her@gmail.com>
2024-04-28 12:53:56 -0500

    added allowed host

--- blog_cms/settings.py
+++ blog_cms/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['blog-cms-django-abaff6e17c2a.herokuapp.com']
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
