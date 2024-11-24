6e9eed485cd775b46a566dda0aa1c466955c5239
lulu <lulu.cao.her@gmail.com>
2024-11-24 13:02:39 -0600

    try again

--- blog_cms/settings.py
+++ blog_cms/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
